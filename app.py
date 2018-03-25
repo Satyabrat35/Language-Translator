1d22d530049e87f58fecf06e5c29f858a05f86a1
Satyabrat35 <satyabrat35@gmail.com>
2018-03-25 06:46:45 +0530

    lang_translater

---
+++ app.py
@@ -0,0 +1,94 @@
+from flask import Flask  , request , render_template ,url_for ,flash
+import json
+import requests
+from watson_developer_cloud import LanguageTranslatorV2 as LanguageTranslator
+
+app = Flask(__name__)
+
+app.secret_key = '\xc9 \x1e\x8d\x04\xed(t\x01g\rN\xa0*G\xb4`\xca\xc5\xb4R\xd4\xa0\x98c\xfb\xbbP\xb5\xb8\xe0\xd9'
+
+language_translator = LanguageTranslator (
+	username = "8cf36c28-6d27-4a70-b143-c4702b31d3e8",
+	password = "NQYn55XI4Q1D"
+	) 
+
+
+@app.route('/')
+def route():
+	return render_template('lang.html')
+
+@app.route('/trn')
+def trns():
+	data2 = {u'character_count': 0, u'translations': [{u'translation': u'Welcome!!'}], u'word_count': 1}
+	return render_template('home.html',data2=data2)
+
+@app.route('/dtc')
+def dtcz():
+	return render_template('home2.html')	
+
+@app.route('/translate',methods=['POST'])
+def trans():
+	if request.method == 'POST':
+		text = request.form.get('text_area')
+		en_to = request.form.get('trans1')
+		to_en = request.form.get('trans2')
+
+
+	print(text)
+	print(en_to)
+	print(to_en)
+
+	if text == '':
+		flash('Enter the text')
+		return render_template('home.html')
+
+	if en_to != 'null' and to_en != 'null':
+		flash('Enter only one translation method')
+		return render_template('home.html')
+
+	if en_to == 'null' and to_en == 'null':
+		flash('Enter one translation method')
+		return render_template('home.html')
+
+	if en_to != 'null':
+		translation = language_translator.translate(
+			text= str(text),
+			model_id= 'en-' + str(en_to))
+		data = json.dumps(translation,indent=2,ensure_ascii=False)
+		data2 = json.loads(data)
+		txt = data2['translations'][0]['translation']
+		return render_template('home.html',data2=data2)
+
+	if to_en != 'null':
+		translation = language_translator.translate(
+			text=str(text),
+			model_id= str(to_en) + '-en')
+		data = json.dumps(translation,indent=2,ensure_ascii=False)
+		data2 = json.loads(data)
+		#flash("Translation: {}".format(data2['translations'][0]['translation']))
+		txt = data2['translations'][0]['translation']
+		return render_template('home.html',data2=data2)
+	
+
+	return render_template('home.html')
+
+@app.route('/detect',methods=['POST'])
+def detection():
+	if request.method == 'POST':
+		text = request.form.get('text_area')
+
+	language = language_translator.identify(str(text))
+	data = json.dumps(language,indent=2)
+	data2 = json.loads(data)
+
+	data3 = data2['languages'][0]['language']
+
+	url = "https://restcountries.eu/rest/v2/alpha/" + str(data3)
+	resp = requests.request('GET',url)
+	country_name = json.loads(resp.content)
+
+	flash("The language is mostly {}".format(country_name['demonym']))
+	return render_template('home2.html')
+
+if __name__ == "__main__":
+	app.run(debug=True)
---
+++ test.py
@@ -0,0 +1,30 @@
+import json
+import requests
+import request
+from watson_developer_cloud import LanguageTranslatorV2 as LanguageTranslator
+
+language_translator = LanguageTranslator (
+	username = "8cf36c28-6d27-4a70-b143-c4702b31d3e8",
+	password = "NQYn55XI4Q1D"
+	) 
+
+translation = language_translator.translate(
+	text='Hello',
+	model_id='es-en')
+
+data = json.dumps(translation,indent=2,ensure_ascii=False)
+print(data)
+data2 = json.loads(data)
+print(data2)
+print(data2['translations'][0]['translation'])
+
+language = language_translator.identify('Gracias')
+data3 = json.dumps(language,indent=2)
+data4 = json.loads(data3)
+print(data4['languages'][0]['language'])
+data5 = data4['languages'][0]['language']
+
+url = "https://restcountries.eu/rest/v2/alpha/" + str(data5)
+resp = requests.request("GET",url)
+cname = json.loads(resp.content)
+print(cname['demonym'])
